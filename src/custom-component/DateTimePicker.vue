879979d18a2af24d6105086737e6be851704e993
zhuliang19961225 <47772586+zhuliang19961225@users.noreply.github.com>
2021-12-28 15:37:36 +0800

    宽度

---
+++ src/custom-component/DateTimePicker.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:"DateTimePicker",
+    props:{
+        propsValue:{
+            type:Object,
+            default:{
+
+            }
+        }
+    }
+}
+</script>
