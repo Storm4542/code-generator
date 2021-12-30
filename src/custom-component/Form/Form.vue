7d675386830cb192794fd2e750fe71b6ea987971
郑天宇 <417002107@qq.com>
2021-12-30 10:36:12 +0800

    更改import 方法

---
+++ src/custom-component/Form/Form.vue
@@ -0,0 +1,35 @@
+<template>
+    <div style="background-color: red">
+        <el-form :ref="propsValue.ref" :label-width="propsValue.labelWidth" :style="innerStyle">
+
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Form",
+    props: {
+        propsValue: {
+            type: Object,
+            default: {
+                ref: 'form',
+                labelWidth: '80px'
+            }
+        },
+        innerStyle: {
+            type: Object,
+            default: {
+                width: '200px',
+                height: '100px',
+                backgroundColor: 'blue'
+            }
+        }
+    }
+}
+;
+</script>
+
+<style scoped>
+
+</style>
